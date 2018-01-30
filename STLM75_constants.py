c64003689dd92e13d414875d3e1a8882ea426186
chisl <info@chisl.io>
2018-01-30 17:22:49 +0100

    Initial commit

---
+++ STLM75_constants.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""STLM75: Digital temperature sensor and thermal watchdog"""
+
+__author__     = "ChISL"
+__copyright__  = "TBD"
+__credits__    = ["ST Microelectronics"]
+__license__    = "TBD"
+__version__    = "Version 0.1"
+__maintainer__ = "https://chisl.io"
+__email__      = "info@chisl.io"
+__status__     = "Test"
+
+#
+#   THIS FILE IS AUTOMATICALLY CREATED
+#    D O     N O T     M O D I F Y  !
+#
+
+class REG:
+	COMMAND = 255
+	TEMP = 0
+	CONF = 1
+	T_OS = 2
+	T_HYS = 2
